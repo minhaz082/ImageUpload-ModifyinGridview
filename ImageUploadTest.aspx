f6e1d289c5fea008f2fd0f93c3a908fd3472d6cc
minhaz082 <minhaz082@hotmail.com>
2015-05-07 12:03:28 +0600

    Image Upload file and links uploaded

---
+++ ImageUploadTest.aspx
@@ -0,0 +1,231 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MegnaSolar.Master" AutoEventWireup="true"
+    EnableEventValidation="false" CodeBehind="ImageUploadTest.aspx.cs" Inherits="EATL.WebClient.CommonUI.ImageUploadTest" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxConTK" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        <fieldset>
+            <legend>Customer Info</legend>
+            <table width="100%">
+                <colgroup>
+                    <col width="15%" />
+                    <col width="32%" />
+                    <col width="6%" />
+                    <col width="15%" />
+                    <col width="32%" />
+                </colgroup>
+                <tr>
+                    <td class="LabelTD_D">
+                        Name :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtCustomerName" runat="server" CssClass="STextBox" Text="" Width="95%"></asp:TextBox>
+                    </td>
+                    <td>
+                    </td>
+                    <td class="LabelTD_D">
+                        Father Name :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtCustomerFatherName" runat="server" CssClass="STextBox" Text=""
+                            Width="95%"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="LabelTD_D">
+                        Spouse Name :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtCustomerSpouseName" runat="server" CssClass="STextBox" Text=""
+                            Width="95%"></asp:TextBox>
+                    </td>
+                    <td>
+                    </td>
+                    <td class="LabelTD_D">
+                        District :
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="dropDLDistrict" runat="server" CssClass="STextBox" Width="95%">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="LabelTD_D" style="display: none;">
+                        Sub District :
+                    </td>
+                    <td style="display: none;">
+                        <asp:DropDownList ID="dropDLSubDistrict" runat="server" CssClass="STextBox" AutoPostBack="true"
+                            Width="95%">
+                        </asp:DropDownList>
+                    </td>
+                    <td style="display: none;">
+                    </td>
+                    <td class="LabelTD_D">
+                        Post Office :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPostOffice" runat="server" CssClass="STextBox" Text="" Width="95%"></asp:TextBox>
+                    </td>
+                    <td>
+                    </td>
+                    <td class="LabelTD_D">
+                        Union :
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="dropDLUnion" runat="server" CssClass="STextBox" Width="95%">
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="LabelTD_D">
+                        Mobile Phone :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtMobilePhone" runat="server" CssClass="STextBox" Text="" Width="95%"></asp:TextBox>
+                    </td>
+                    <td>
+                    </td>
+                    <td class="LabelTD_D">
+                        Village :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtVillage" runat="server" CssClass="STextBox" Text="" Width="95%"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="LabelTD_D">
+                        National ID :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtNationalID" runat="server" CssClass="STextBox" Text="" Width="95%"></asp:TextBox>
+                    </td>
+                    <td>
+                    </td>
+                    <td class="LabelTD_D">
+                        Home Phone :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtHomePhone" runat="server" CssClass="STextBox" Text="" Width="95%"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="LabelTD_D">
+                        Pass Book No :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPassBookNo" runat="server" CssClass="STextBox" Text="" Width="95%"></asp:TextBox>
+                    </td>
+                    <td>
+                    </td>
+                    <td class="LabelTD_D">
+                        Customer No :
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtCustomerNo" runat="server" CssClass="STextBox" Text="" Width="95%"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="LabelTD_D">
+                        &nbsp;
+                    </td>
+                    <td>
+                        &nbsp;
+                    </td>
+                    <td>
+                    </td>
+                    <td class="LabelTD_D">
+                    </td>
+                    <td>
+                    </td>
+                </tr>
+            </table>
+        </fieldset>
+    </div>
+    <div id="CustomerImage">
+        <fieldset>
+            <legend>Customer Picture</legend>
+            <table width="100%">
+                <colgroup>
+                    <col width="15%" />
+                    <col width="33%" />
+                    <col width="4%" />
+                    <col width="20%" />
+                    <col width="28%" />
+                </colgroup>
+                <tr>
+                    <td colspan="5">
+                        <asp:CheckBox ID="CheckBox" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5">
+                        <asp:Image ID="ImgCustomerPicture" runat="server" Width="220px" Height="200px" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5">
+                        <asp:FileUpload ID="file" runat="server" Width="80px" />
+                        <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" /><br />
+                        <asp:Label ID="lblImagePath" runat="server" Text="" Visible="false"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" AllowPaging="True"
+                OnRowCancelingEdit="GridView1_RowCancelingEdit" DataKeyNames="IID" CellPadding="4"
+                OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" ForeColor="#333333">
+                <Columns>
+                    <asp:TemplateField HeaderText="Sl." HeaderStyle-Width="30px">
+                        <ItemTemplate>
+                            <asp:Label ID="lblSRNO" runat="server" Text='<%#Container.DataItemIndex+1 %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="IID" HeaderStyle-Width="200px" Visible="false">
+                        <ItemTemplate>
+                            <asp:Label ID="lblIID" runat="server" Text='<%# Eval("IID") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="CustomerID" HeaderStyle-Width="200px" Visible="false">
+                        <ItemTemplate>
+                            <asp:Label ID="lblCustomerID" runat="server" Text='<%# Eval("CustomerID") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="ImageLocation" HeaderStyle-Width="200px">
+                        <ItemTemplate>
+                            <asp:Label ID="lblImageLocation" runat="server" Text='<%# Eval("ImageLocation") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Image" HeaderStyle-Width="105px">
+                        <ItemTemplate>
+                            <asp:Image ID="Image1" runat="server" ImageUrl='<%# Eval("ImageLocation") %>' Height="80px"
+                                Width="100px" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:Image ID="img_user" runat="server" ImageUrl='<%# Eval("ImageLocation") %>' Height="80px"
+                                Width="100px" /><br />
+                            <asp:FileUpload ID="FileUpload1" runat="server" />
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderStyle-Width="150px">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="LkB1" runat="server" CommandName="Edit">Edit</asp:LinkButton>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:LinkButton ID="LkB2" runat="server" CommandName="Update">Update</asp:LinkButton>
+                            <asp:LinkButton ID="LkB3" runat="server" CommandName="Cancel">Cancel</asp:LinkButton>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </fieldset>
+    </div>
+    <%--<div>
+        <fieldset>
+            <asp:Button ID="btnSave" runat="server" Text="Save" Width="100px" ForeColor="#05799e"
+                Font-Bold="true" OnClick="btnSave_Click" />
+            <asp:Label ID="lblWarning" runat="server" Text=""></asp:Label>
+        </fieldset>
+    </div>--%>
+</asp:Content>
---
+++ ImageUploadTest.aspx.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.IO;
+using EATL.DAL;
+using EATL.BLL.Facade;
+using System.Data;
+
+namespace EATL.WebClient.CommonUI
+{
+    public partial class ImageUploadTest : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=192.168.1.4;Initial Catalog=MeghnaSolar;User ID=sa;Password=dba@123");
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ImgCustomerPicture.Visible = false;
+                //BindGridView();
+                loadGridviewWithImage();
+            }
+        }               
+        
+        protected void btnUpload_Click_Old(object sender, EventArgs e)
+        {
+        //    if (file.HasFile)
+        //    {
+        //        string filename = file.FileName;
+        //        string filepath = Server.MapPath("~/Images/");
+        //        lblImagePath.Text = filepath + filename;
+        //        int fileLength = file.PostedFile.ContentLength;
+        //        string fileExtention = Path.GetExtension(filename);
+        //        fileExtention = fileExtention.ToLower();
+        //        string link = "~/Images/" + filename;
+        //        //string fileName = Path.Combine(@"D:\ImageUpload", filename);    file name given exact location in the dish of user
+
+        //        if (fileLength < 1048576)
+        //        {
+        //            if (fileExtention == ".jpg" || fileExtention == ".png" || fileExtention == ".gif" || fileExtention == ".bmp")
+        //            {
+        //                file.SaveAs(filepath + filename);
+        //                ImgCustomerPicture.Visible = true;
+        //                ImgCustomerPicture.ImageUrl = "~/Images/" + filename;
+
+
+        //                string[] filePaths = Directory.GetFiles(Server.MapPath("~/Images/"));
+        //                List<ListItem> files = new List<ListItem>();
+        //                foreach (string filePath in filePaths)
+        //                {
+        //                    string fileName = Path.GetFileName(filePath);
+        //                    string customerName = "Abul";
+
+        //                    files.Add(new ListItem(customerName, "~/Images/" + fileName));
+        //                }
+        //                //grvCustomerPicture.DataSource = files;
+        //                //grvCustomerPicture.DataBind();
+        //                GridView1.DataSource = files;
+        //                GridView1.DataBind();
+        //            }
+        //            else
+        //            {
+        //                Response.Write("<script>alert('Only image files are allowed');</script>");
+        //            }
+        //        }
+        //        else
+        //        {
+        //            Response.Write("<script>alert('Max file size allowed is 1 MB');</script>");
+        //        }
+
+        //    }
+        //    else
+        //    {
+        //        Response.Write("<script>alert('Please Select File To Upload');</script>");
+        //    }
+        }
+
+        
+        protected CustomerImage CreateCustomerImage()
+        {
+            CustomerImage customerImage = new CustomerImage();
+
+            //customerImage.CustomerID = 1;
+            //customerImage.ImageLocation = lblImagePath.Text;
+            //customerImage.CreateBy = Convert.ToInt64(Session["UserID"]);
+            //customerImage.CreateDate = GeneralConfigurationFacade.GetServerDateTime();
+            //customerImage.UpdateBy = Convert.ToInt64(Session["UserID"]);
+            //customerImage.Updatedate = GeneralConfigurationFacade.GetServerDateTime();
+            //customerImage.IsRemove = false;
+            return customerImage;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            //if (CheckBox.Checked)
+            //{
+            //    using (LeasingFacade _facade = new LeasingFacade())
+            //    {
+            //        CustomerImage customerImage = CreateCustomerImage();
+            //        _facade.AddCustomerImage(customerImage);
+            //        if (customerImage.IID > 0)
+            //        {
+            //            lblWarning.Text = "Data successfully saved...";
+            //            lblWarning.ForeColor = System.Drawing.Color.Green;
+            //        }
+            //        else
+            //        {
+            //            lblWarning.Text = "Data not saved...";
+            //            lblWarning.ForeColor = System.Drawing.Color.Red;
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    lblWarning.Text = "Image not saved...click the checkbox ";
+            //    lblWarning.ForeColor = System.Drawing.Color.Red;
+            //}
+            
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //using (LeasingFacade _facade = new LeasingFacade())
+            //{
+
+            //    CustomerImage customerImage = CreateCustomerImage();
+            //    CustomerImage customerImageUpdate = _facade.GetCustomerImageByIID(customerImage.IID);
+            //    customerImageUpdate.ImageLocation = lblImagePath.Text;
+            //    customerImageUpdate.UpdateBy = Convert.ToInt64(Session["UserID"].ToString()); ;
+            //    customerImageUpdate.Updatedate = DateTime.Now;
+            //    if (customerImageUpdate != null)
+            //    {
+            //        _facade.UpdateCustomerImage(customerImageUpdate);
+                    
+            //        lblWarning.Text = "Data successfully updated...";
+            //        lblWarning.ForeColor = System.Drawing.Color.Green;
+            //    }
+            //    else
+            //    {
+            //        lblWarning.Text = "Data not updated...";
+            //        lblWarning.ForeColor = System.Drawing.Color.Red;
+            //    }
+            //}
+            
+        }
+
+        ///////////////////////////(working)
+
+        private void loadGridviewWithImage()
+        {
+            DataTable dt = new DataTable();
+
+            string strQuery = "select * from CustomerImage order by IID";
+
+            SqlCommand cmd = new SqlCommand(strQuery);
+            SqlConnection con = new SqlConnection(@"Data Source=192.168.1.4;Initial Catalog=MeghnaSolar;User ID=sa;Password=dba@123");
+            SqlDataAdapter sda = new SqlDataAdapter();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+
+            try
+            {
+                con.Open();
+                sda.SelectCommand = cmd;
+                sda.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+            finally
+            {
+                con.Close();
+                sda.Dispose();
+                con.Dispose();
+            }
+        }
+        
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+
+            if (file.PostedFile != null)
+            {
+                string FileName = file.FileName; //Path.GetFileName(file.PostedFile.FileName);
+
+                //Save files to disk
+
+                file.SaveAs(Server.MapPath("~/Images/" + FileName));
+
+                //Add Entry to DataBase               
+
+                SqlConnection con = new SqlConnection(@"Data Source=192.168.1.4;Initial Catalog=MeghnaSolar;User ID=sa;Password=dba@123");
+                string strQuery = "insert into CustomerImage (CustomerID,ImageLocation,CreateBy,CreateDate,UpdateBy,Updatedate,IsRemove)" + " values(@CustomerID,@ImageLocation,@CreateBy,@CreateDate,@UpdateBy,@Updatedate,@IsRemove)";
+
+                SqlCommand cmd = new SqlCommand(strQuery);
+                //cmd.Parameters.AddWithValue("@FileName", FileName);               
+                
+                cmd.Parameters.AddWithValue("@CustomerID", Convert.ToInt64(Session["UserID"]));                
+                cmd.Parameters.AddWithValue("@ImageLocation", "/Images/" + FileName);
+                cmd.Parameters.AddWithValue("@CreateBy", Convert.ToInt64(Session["UserID"]));
+                cmd.Parameters.AddWithValue("@CreateDate", GeneralConfigurationFacade.GetServerDateTime());
+                cmd.Parameters.AddWithValue("@UpdateBy", Convert.ToInt64(Session["UserID"]));
+                cmd.Parameters.AddWithValue("@Updatedate", GeneralConfigurationFacade.GetServerDateTime());
+                cmd.Parameters.AddWithValue("@IsRemove", "False");
+
+                
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = con;
+
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                catch (Exception ex)
+                {
+                    Response.Write(ex.Message);
+                }
+
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                    loadGridviewWithImage();
+                }
+
+            }
+
+        }        
+
+        //////////////////////////(wprking)
+
+        protected void BindGridView()
+    {
+        DataTable dt = new DataTable();
+        SqlDataAdapter da = new SqlDataAdapter("Select IID,CustomerID,ImageLocation,CreateBy,CreateDate,UpdateBy,Updatedate,IsRemove from CustomerImage", con);
+        con.Open();
+        da.Fill(dt);
+        con.Close();
+
+        if (dt.Rows.Count > 0)
+        {
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+        // edit event
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        BindGridView();
+    }
+        // update event
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        //find student id of edit row
+        string id = GridView1.DataKeys[e.RowIndex].Value.ToString();
+        // find values for update
+        TextBox name = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtImageLocation");
+        FileUpload FileUpload1 = (FileUpload)GridView1.Rows[e.RowIndex].FindControl("FileUpload1");
+        string path = "~/Images/";
+        string ImageLocation = "/Images/" + FileUpload1.FileName;
+        if (FileUpload1.HasFile)
+        {
+            path += FileUpload1.FileName;
+            //save image in folder
+            //FileUpload1.SaveAs(MapPath(path));
+            FileUpload1.SaveAs(Server.MapPath("~/Images/" + FileUpload1.FileName));
+        }
+        else
+        {
+            // use previous user image if new image is not changed
+            Image img = (Image)GridView1.Rows[e.RowIndex].FindControl("img_user");
+            path = img.ImageUrl;
+        }
+        //IID,CustomerID,ImageLocation,CreateBy,CreateDate,UpdateBy,Updatedate,IsRemove
+        SqlCommand cmd = new SqlCommand("update CustomerImage set ImageLocation='" + ImageLocation + "' where IID=" + id + "", con);
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+        
+        GridView1.EditIndex = -1;
+        BindGridView();
+
+    }
+        // cancel edit event
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindGridView();
+        }
+
+     }
+}
